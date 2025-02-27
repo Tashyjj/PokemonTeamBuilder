96f8932a10402ccb07b4827c66a99db8d17a3fea
Tashyjj <jamal.m0917@gmail.com>
2025-02-27 16:54:49 -0500

    style changes, fonts etc

--- App.js
+++ App.js
@@ -9,7 +9,15 @@ const app = express();
 app.engine('mustache', mustacheExpress(__dirname + '/views/partials'));
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: function (res, filePath) {
+    if (filePath.endsWith('.woff2')) {
+      res.set('Content-Type', 'font/woff2');
+    }else if (filePath.endsWith('.ttf')) {
+      res.set('Content-Type', 'font/ttf');
+    }
+  }
+}));
 app.use(express.urlencoded({ extended: false }));
 
 const teamController = require('./controllers/teamController');
